400c56376c1535a8c882c880b8a21f9ea3009b00
Frank Moreno <Frank.Moreno95@gmail.com>
2024-09-12 22:49:53 -0500

    Add function to convert APIGatewayResult to ServerResponse

--- src/APIGatewayProxyEvent/convertAPIGatewayProxyEventResultToResponse.ts
+++ src/APIGatewayProxyEvent/convertAPIGatewayProxyEventResultToResponse.ts
@@ -1,2 +1,23 @@
-export function convertAPIGatewayProxyEventResultToResponse(): APIGate {}
+import { APIGatewayProxyResult } from "aws-lambda";
+import { ServerResponse } from "http";
 
+export function convertAPIGatewayProxyEventResultToResponse(
+  result: APIGatewayProxyResult,
+  response: ServerResponse
+) {
+  response.statusCode = result.statusCode;
+
+  if (result.headers) {
+    Object.entries(result.headers).forEach(([key, value]) => {
+      response.appendHeader(key, String(value));
+    });
+  }
+
+  if (result.multiValueHeaders) {
+    Object.entries(result.multiValueHeaders).forEach(([key, value]) => {
+      response.appendHeader(key, value.map(String));
+    });
+  }
+
+  response.end(result.body ?? null);
+}
